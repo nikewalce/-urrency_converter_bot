62bb60c8681cdfdbb46271b062833b1681af80b6
nikewalce <nikewalce1@gmail.com>
2026-01-15 16:18:14 +0300

    .

--- src_bot/currency_service.py
+++ src_bot/currency_service.py
@@ -1,11 +1,11 @@
 class CurrencyService:
     RATES = {
-        "USD_RUB": 80.33,
-        "RUB_USD": 1 / 80.33,
-        "THB_RUB": 2.45,
-        "RUB_THB": 1 / 2.45,
-        "USD_THB": 32.48,
-        "THB_USD": 1 / 32.48
+        "USD_RUB": 78.65,
+        "RUB_USD": 1 / 78.65,
+        "THB_RUB": 2.51,
+        "RUB_THB": 1 / 2.51,
+        "USD_THB": 31.39,
+        "THB_USD": 1 / 31.39
     }
 
     @classmethod
